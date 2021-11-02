3108557ec8a445a7910aa44204d6777bc100cc85
Adrian Ruiz <adrianruiz.981031@gmail.com>
2021-11-02 08:41:57 -0600

    se agrega nueva tarea

--- js/to-do.js
+++ js/to-do.js
@@ -1,30 +1,61 @@
 const tarea = document.getElementById("tarea")
 const add = document.getElementById("add")
-const pendiente = document.getElementsByClassName("pendiente")
-
+const pendiente = document.getElementsByClassName("pendiente")[0]
+const terminada = document.getElementsByClassName('terminada')[0]
 
 var tareas = []
+var finalizadas = []
 
 add.onclick = () =>{
     var valor = tarea.value
+    
+    addCard(valor, pendiente, true)
+    saveCard(valor)
+    tarea.value = ""
+    
+}
+function saveCard(valor){
     tareas.push(valor)
-    var nuevo = document.createElement('div')
-    var texto = document.createElement('p')
+}
+function endCard(valor){
+    finalizadas.push(valor) 
+}
+function addCard(valor, listado, botones){
+    var card = botones ? cardButtons(newCard(valor)) : newCard(valor)
+    listado.appendChild(card)
+}
+function cardButtons(card){
     var add = document.createElement('button')
     var remove = document.createElement('button')
+    
+    add.innerHTML = "&#9989;"
+    add.classList = "card-button-add"
+    remove.innerHTML = "&#10060;"
+    remove.classList = "card-button-remove"
+
+    card.appendChild(add)
+    card.appendChild(remove)
+
+
+    add.onclick = ()=>{
+        let nCard = card.firstChild
+        endCard(nCard.textContent)
+        addCard(nCard.textContent, terminada, false)
+        var padre = card.parentElement
+        padre.removeChild(card)
+    }
+
+    return card
+}
+
+function newCard(valor){
+    var nuevo = document.createElement('div')
+    var texto = document.createElement('p')
     nuevo.classList = "card"
     texto.innerHTML = valor
     texto.classList = "card-title"
-    add.innerHTML = "Check"
-    add.classList = "card-button-add"
-    remove.innerHTML = "Remove"
-    remove.classList = "card-button-remove"
     nuevo.appendChild(texto)
-    nuevo.appendChild(add)
-    nuevo.appendChild(remove)
-    pendiente[0].appendChild(nuevo)
-    tarea.value = ""
-    
+    return nuevo
 }
 
 function click(){
